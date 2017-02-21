9a27017b64b349a825f0a6d6a2a916071d7d13d6
Dimitrii DULGHER <ddulgher@e1p93.chisinau.42.fr>
2017-02-21 16:47:19 +0200

    Update.

--- mandelbrot_set.c
+++ mandelbrot_set.c
@@ -6,14 +6,30 @@
 void *mlx;
 void *win;
 
+void ft_makecolors(int max, int colors[])
+{
+  int i;
+  int basecolor;
+
+  basecolor = 1651275;
+  i = 0;
+  while (i < max)
+  {
+    colors[i] = basecolor + i * 10;
+    i++;
+  }
+}
+
 int   main(int argc, char *argv[])
 {
   int height, width;
   int max;
 
-  max = 50;
+  max = 100;
   height = 480;
   width = 640;
+  int colors[max];
+  ft_makecolors(max, colors);
   mlx = mlx_init();
   win = mlx_new_window(mlx, width, height, "Fractal");
   for (int row = 0; row < height; row++)
@@ -33,7 +49,7 @@ int   main(int argc, char *argv[])
         iteration++;
       }
       if (iteration < max)
-        mlx_pixel_put(mlx, win, col, row, 0xFFFFFF);
+        mlx_pixel_put(mlx, win, col, row, colors[iteration]);
       else
         mlx_pixel_put(mlx, win, col, row, 0x000000);
     }
